7d410d80288ae45ad4b91e4d64a045dc1df78a07
林政憲 <ab0923223452@gmail.com>
2021-04-29 12:26:56 +0800

    week3 hw

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,8 +1,8 @@
 function capitalize(str) {
-    var first = str[0]
-    var newFirst = first.toUpperCase()
-    newstr = str.replace(/^[a-z]/,newFirst)
-    return  newstr
+  const first = str[0]
+  const newFirst = first.toUpperCase()
+  const newstr = str.replace(/^[a-z]/, newFirst)
+  return newstr
 }
 
-console.log(capitalize('adick'),capitalize('aick'),capitalize('9hello'));
+console.log(capitalize('adick'), capitalize('aick'), capitalize('9hello'))
